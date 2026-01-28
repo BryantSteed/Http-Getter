4506b33da2bcc3d70f06e5dd445ab78a42a268db
Bryant <bryant.steed@gmail.com>
2026-01-28 12:31:04 -0700

    initial commit

---
+++ src/Getter.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp_stuff;
+
+using System.Net.Http;
+class Getter
+{
+
+    private static void Main(string[] args)
+    {
+        string inputUrl = args[0];
+        Console.WriteLine($"Sending Request to Input URL: {inputUrl}");
+        SendRequest(inputUrl);
+    }
+
+    private static void SendRequest(string url)
+    {
+        HttpClient client = new HttpClient();
+        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+        Task<HttpResponseMessage> task = client.SendAsync(request);
+        task.Wait();
+        HttpResponseMessage response = task.Result;
+        HttpContent content = response.Content;
+        Task<string> stream = content.ReadAsStringAsync();
+        stream.Wait();
+        Console.WriteLine(stream.Result);
+    }
+}
